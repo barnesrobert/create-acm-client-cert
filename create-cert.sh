98e912b230144450d5e0359fe165e245ef6ed888
Rob Barnes <barnesrobert@users.noreply.github.com>
2020-12-22 08:20:41 -0500

    Update create-cert.sh

--- create-cert.sh
+++ create-cert.sh
@@ -2,21 +2,32 @@
 # https://docs.aws.amazon.com/vpn/latest/clientvpn-admin/cvpn-getting-started.html#cvpn-getting-started-certs
 # https://docs.aws.amazon.com/vpn/latest/clientvpn-admin/client-authentication.html#mutual
 
-#./easyrsa build-server-full aws.local nopass
+# TODO Get server keys from S3...
 
-user=USER_NAME_FOR_EACH_CLIENT_CERT
+export domain=aws.local
+export user=TEST-USER
+export EASYRSA_BATCH=1
 
-./easyrsa build-client-full $user.domain.tld nopass
+
+sudo yum install openssl -y
+git clone https://github.com/OpenVPN/easy-rsa.git
+cd easy-rsa/easyrsa3
+./easyrsa init-pki
+./easyrsa build-ca nopass
+./easyrsa build-server-full $domain nopass
+
+./easyrsa build-client-full $user.$domain nopass
 
 mkdir ~/$user/
 cp pki/ca.crt ~/$user/
-cp pki/issued/aws.local.crt ~/$user/
-cp pki/private/aws.local.key ~/$user/
-cp pki/issued/$user.aws.local.crt ~/$user
-cp pki/private/$user.aws.local.key ~/$user/
+cp pki/issued/$domain.crt ~/$user/
+cp pki/private/$domain.key ~/$user/
+cp pki/issued/$user.$domain.crt ~/$user
+cp pki/private/$user.$domain.key ~/$user/
 cd ~/$user/
 
 
 
-aws acm import-certificate --certificate fileb://aws.local.crt --private-key fileb://aws.local.key --certificate-chain fileb://ca.crt
-aws acm import-certificate --certificate fileb://$user.aws.local.crt --private-key fileb://$user.aws.local.key --certificate-chain fileb://ca.crt
+aws acm import-certificate --certificate fileb://$domain.crt --private-key fileb://$domain.key --certificate-chain fileb://ca.crt --tags Key=Name,Value=$domain
+sleep 2
+aws acm import-certificate --certificate fileb://$user.$domain.crt --private-key fileb://$user.$domain.key --certificate-chain fileb://ca.crt --tags Key=Name,Value=$user
