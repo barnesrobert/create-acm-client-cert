8443cb4bace9115d0a8c82880873dc0b8a5ae279
Rob Barnes <barnesrobert@users.noreply.github.com>
2020-12-22 10:09:34 -0500

    Update create-cert.sh

--- create-cert.sh
+++ create-cert.sh
@@ -24,16 +24,18 @@ cp pki/private/$USER.$DOMAIN.key ~/$USER/
 cd ~/$USER/
 
 
-
+# Import the server certificate into ACM.
 DOMAIN_CERT_ARN=$(aws acm import-certificate --certificate fileb://$DOMAIN.crt --private-key fileb://$DOMAIN.key --certificate-chain fileb://ca.crt --tags Key=Name,Value=$DOMAIN --query CertificateArn --output text)
 
 # Calling the import-certificate API quickly in succession can create a throttling error.
 sleep 2
 
+# Import the user certificate into ACM.
 USER_CERT_ARN=$(aws acm import-certificate --certificate fileb://$USER.$DOMAIN.crt --private-key fileb://$USER.$DOMAIN.key --certificate-chain fileb://ca.crt --tags Key=Name,Value=$USER --query CertificateArn --output text)
 
 # Conditionally create the log group and stream.
 #aws logs create-log-group --log-group-name clientvpn
 #aws logs create-log-stream --log-group-name clientvpn --log-stream-name $DOMAIN/$USER
 
-aws ec2 create-client-vpn-endpoint --client-cidr-block 10.5.0.0/22 --server-certificate-arn $DOMAIN_CERT_ARN --authentication-options Type=certificate-authentication,MutualAuthentication={ClientRootCertificateChainArn=$USER_CERT_ARN} --connection-log-options Enabled=true,CloudwatchLogGroup=clientvpn,CloudwatchLogStream=$DOMAIN/$USER --tag-specifications ResourceType=client-vpn-endpoint,Tags=[{Key=Name,Value=$USER}]
+# Create the client VPN endpoint using mutual certificate authentication.
+aws ec2 create-client-vpn-endpoint --client-cidr-block 10.5.0.0/22 --server-certificate-arn $DOMAIN_CERT_ARN --authentication-options Type=certificate-authentication,MutualAuthentication={ClientRootCertificateChainArn=$USER_CERT_ARN} --connection-log-options Enabled=true,CloudwatchLogGroup=clientvpn,CloudwatchLogStream=$DOMAIN/$USER --tag-specifications ResourceType=client-vpn-endpoint,Tags="[{Key=Name,Value=$DOMAIN/$USER}]"
